d6bbd39645f74a6a9d0863aa9091885ce649eb4e
Bora <gundogubora24@gmail.com>
2024-02-24 18:40:29 +0300

    Initial Commit

---
+++ FOLY_Info/FOLY_InfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FOLY_InfoApp.swift
+//  FOLY_Info
+//
+//  Created by Bora Gündoğu on 24.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct FOLY_InfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
